4c423d4b8f274bd868167c56115480cb9f291e6f
Tory Peterschild <torypeterschild@Torys-MacBook-Pro.local>
2016-07-18 20:34:21 -0400

    small changes

--- parser.py
+++ parser.py
@@ -1,4 +1,10 @@
 from textblob import TextBlob
+import random
+
+
+EOS = ['.', '?', '!']
+
+
 
 
 with open('static/corpus000.txt') as f:
@@ -8,3 +14,5 @@ blob = TextBlob(content.decode('utf-8'))
 
 for sentence in blob.sentences:
     print(sentence)
+
+print(blob.ngrams(n=3))
